ed3f2acd6d95a55388e42f58287c406fcd690d15
Artem Struts <strutsart7@gmail.com>
2021-10-05 12:53:32 +0300

    add login page

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('chat'),
-);
+export default () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('chat'),
+  );
+};
