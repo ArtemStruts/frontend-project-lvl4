9eee50d6acdc01d8c3d9aee718fcc545b6f55d3f
Artem Struts <strutsart7@gmail.com>
2021-10-20 20:51:58 +0300

    test 8

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -17,8 +17,10 @@ const NonRemovableChannel = ({ id, name, handleChange }) => {
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
   return (
     <Button className="w-100 rounded-0 text-start" variant={getButtonVariant(id, currentChannelId)} type="button" onClick={handleChange(id)}>
-      <span className="me-1">#</span>
-      {name}
+      <span className="me-1">
+        #
+        {name}
+      </span>
     </Button>
   );
 };
@@ -39,8 +41,10 @@ const RemovableChannel = ({ id, name, handleChange }) => {
   return (
     <Dropdown as={ButtonGroup} className="d-flex">
       <Button className="w-100 rounded-0 text-start" variant={getButtonVariant(id, currentChannelId)} type="button" onClick={handleChange(id)}>
-        <span className="me-1">#</span>
-        {name}
+        <span className="me-1">
+          #
+          {name}
+        </span>
       </Button>
 
       <Dropdown.Toggle split variant={getButtonVariant(id, currentChannelId)} />
@@ -74,6 +78,7 @@ const Channels = () => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            <span className="visuallyHidden">+</span>
           </svg>
         </Button>
       </div>
